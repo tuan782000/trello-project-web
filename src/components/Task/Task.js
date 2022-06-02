7528897f096ee06e759bb7be133eec96933154dc
tuan782000 <thaituan7820@gmail.com>
2022-06-02 12:06:56 +0700

    Clean react Architecture.

---
+++ src/components/Task/Task.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './Task.scss';
+
+function Task() {
+    return (
+        <li className="task-item">
+            <img src="https://scontent.fsgn5-11.fna.fbcdn.net/v/t1.18169-9/28166849_1049354731880931_834437487891460936_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a4a2d7&_nc_ohc=NQnb50tD7T0AX_cQ6nx&_nc_ht=scontent.fsgn5-11.fna&oh=00_AT9odX5SIdbabsUsUBGyXL4Yyh9Zqeqba4Olq4lMMbuc5Q&oe=62BD2DA8" alt="tuannguyen" />
+            Title: TUAN NGUYEN
+        </li >
+    )
+}
+
+export default Task
