32bd4645d20f83e494aaa48af67c9ca299e361d1
tuan782000 <thaituan7820@gmail.com>
2022-06-04 16:43:13 +0700

    Fix bug missing add card action

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -107,7 +107,7 @@ function Column(props) {
             <Dropdown.Toggle id="dropdown-basic" size ="sm" className="dropdown-btn"/>
 
             <Dropdown.Menu>
-              <Dropdown.Item>Add Card...</Dropdown.Item>
+              <Dropdown.Item onClick={toggleOpenNewCardForm}>Add Card...</Dropdown.Item>
               <Dropdown.Item onClick={toggleShowConfirmModal}>Remove Column</Dropdown.Item>
               <Dropdown.Item>Move all card in this column (beta)...</Dropdown.Item>
               <Dropdown.Item>Archive all card in this column (beta)...</Dropdown.Item>
